f36c62703a4c62a190ec2f2ed4aeec50339e14aa
snotebook <>
2019-08-18 15:28:58 +0200

    fixed: added problem solutions

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -20,8 +19,32 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        # input 1,225,225 //(225-5)/1 +1 = 221 // output 32,221,221
+        # output after maxpool: 32,110,110
+        self.conv1 = nn.Conv2d(1, 32, 3) 
+        self.conv1_bn2 = nn.BatchNorm2d(32)
+        # output after maxpool: 64,54,54
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv2_bn2 = nn.BatchNorm2d(64)
+        # output after maxpool: 128,26,26
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv3_bn2 = nn.BatchNorm2d(128)
+        # output after maxpool: 256,12,12
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.conv4_bn2 = nn.BatchNorm2d(256)
+        # Maxpooling layer
+        self.pool = nn.MaxPool2d(2, 2)
         
+        # Fully connected layers
+        self.fc1 = nn.Linear(12*12*256, 3000)
+        self.fc1_bn1 = nn.BatchNorm1d(3000)
+        self.fc2 = nn.Linear(3000, 1000)
+        self.fc2_bn1 = nn.BatchNorm1d(1000)
+        self.fc3 = nn.Linear(1000,136)
+        
+        self.dropout = nn.Dropout(0.15)
+
+
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
@@ -30,8 +53,14 @@ class Net(nn.Module):
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
+        x = self.dropout(self.pool(F.relu(self.conv1_bn2(self.conv1(x)))))
+        x = self.dropout(self.pool(F.relu(self.conv2_bn2(self.conv2(x)))))
+        x = self.dropout(self.pool(F.relu(self.conv3_bn2(self.conv3(x)))))
+        x = self.dropout(self.pool(F.relu(self.conv4_bn2(self.conv4(x)))))
+        # Flatten layer
+        x = x.view(x.size(0), -1)
+        x = self.dropout(F.relu(self.fc1_bn1(self.fc1(x))))
+        x = self.dropout(F.relu(self.fc2_bn1(self.fc2(x))))
+        x = self.fc3(x) 
         # a modified x, having gone through all the layers of your model, should be returned
         return x
